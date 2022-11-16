e411e48980645ae1c90c0fd0a9be14639e1cb15b
Juan Bejarano <juanjbejarano.99@gmail.com>
2022-11-16 10:44:44 -0300

    cors added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ import { connectDB } from './database.js';
 import {PORT} from './config.js'
 import app from './app.js'
 
+import cors from 'cors'
+
+const whiteList = ["http://localhost:3000/","http://crersolucionestecno.ar/"]
+
+app.use(cors({
+  origin:[whiteList]
+}));
+
+
 connectDB();
 app.listen(PORT);
 console.log('Server is running on Port: ',PORT);
